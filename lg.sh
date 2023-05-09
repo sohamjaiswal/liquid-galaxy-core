036bd6d0293c4b5bee6514cf8c66210ce5e5ccae
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-05-09 12:47:07 +0530

    test: compile src dir

--- lg.sh
+++ lg.sh
@@ -329,7 +329,7 @@ EOM
 mkdir -p $HOME/.config/autostart/
 echo -e "[Desktop Entry]\nName=LG\nExec=bash "$HOME"/earth/scripts/launch-earth.sh\nType=Application" > $HOME"/.config/autostart/lg.desktop"
 
-git clone $GIT_URL
+echo "$HOME"/write-event $GIT_FOLDER_NAME/input_event/write-event.c 
 gcc -m32 -o "$HOME"/write-event $GIT_FOLDER_NAME/input_event/write-event.c 
 sudo chmod 0755 "$HOME"/write-event
 
